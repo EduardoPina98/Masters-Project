b743213382c0baa9d973c2bfc4b5c577a165dada
Eduardo Pina <eduardomanuel.pina@gmail.com>
2025-05-18 11:27:09 +0100

    Added verification articles and justification for variables (not all)

--- machine_learning.py
+++ machine_learning.py
@@ -119,69 +119,106 @@ def preprocess_data(df):
         'avg_waking_respiration': 0.05
     }
 
-    df['cvd_risk_score'] = 0
+    df['cvd_risk_score'] = 0.0
     df['cvd_risk'] = 'NaN'
 
     for index, row in df.iterrows():
         score = 0.0
 
-        # Combination 1: vo2max + bmi (validated)
+        # Combination 1: vo2max + bmi (validated) BMI: Association between obesity categories with cardiovascular disease and its related risk factors in the MASHAD cohort study population, Journal of Clinical Laboratory Analysis, 2020
         # Articles: Cardiorespiratory fitness, body mass index and mortality: a systematic review and meta-analysis
-        if row['vo2_max_precise'] < 38.0 and (row['bmi'] < 18.5 or row['bmi'] > 24.9):
+        # Justification: High fitness reduces CVD risk significantly even with high BMI.
+        # Weight article and Justification: Same article. Vo2max and BMI together strongly predict cardiovascular fitness and obesity-related cardiovascular risk.
+        if row['vo2_max_precise'] < 41.7 and (row['bmi'] < 18.5 or row['bmi'] > 24.9):
             score += weights['vo2max_bmi']
 
-        # Combination 2: vo2max + sleep time (validated)
-        # Article: The importance of cardiorespiratory fitness and sleep duration in early CVD prevention: BMI, resting heart rate and questions about sleep patterns are suggested in risk assessment of young adults, 18–25 years
-        if row['vo2_max_precise'] < 38.0 and (row['sleep_time_sec'] < 21600  or row['sleep_time_sec'] > 28800):
-            score += weights['vo2max_sleep_time']
+        # Combination 2: vo2max + sleep time (validated) sleep validate: Association of sleep duration and cardiovascular events, European Heart Journal, 2020
+        # Article: Joint association of physical activity and sleep duration with risk of all-cause and cause-specific mortality: a population-based cohort study using accelerometry
+        # Justification: High fitness mitigates CVD risk associated with unhealthy sleep of short or long durations.
+        # Weight article and Justification: Same article. Both VO₂max (fitness) and sleep duration independently affect cardiovascular recovery; combined effect enhances prediction.
 
-        # Combination 3: bmi + resting heart rate (validated)
-        # Article: Effect of resting heart rate on the risk of metabolic syndrome in adults: a dose–response meta-analysis
-        if (row['bmi'] < 18.5 or row['bmi'] > 24.9) and row['resting_heart_rate'] > 70:
+        if row['vo2_max_precise'] < 41.7 and (row['sleep_time_sec'] < 21600  or row['sleep_time_sec'] > 28800):
+            score += weights['vo2max_sleep_time']
+        
+        ## Effect of resting heart rate on the risk of metabolic syndrome in adults: a dose–response meta-analysis (T2D) article
+        # Combination 3: bmi + resting heart rate (Validated Value Article) RHR: Association of resting heart rate and physical activity with cardiovascular mortality: A population-based cohort study of Korean adults Journal of sports sciences, 2024
+        # Article: Exercise capacity and body mass index - important predictors of change in resting heart rate, BMC Cardiovascular Disorders 2019: High Resting Heart Rate and High BMI Predicted Severe Coronary Atherosclerosis Burden in Patients With Stable Angina Pectoris by SYNTAX Score, Angiology 2018
+        # Weight article and Justification: Same first article This study found that increases in BMI were correlated with increases in RHR, even among individuals with normal BMI ranges. The findings suggest that BMI and RHR are interrelated and that their combined elevation may contribute to increased cardiovascular risk.
+        if (row['bmi'] < 18.5 or row['bmi'] > 24.9) and row['resting_heart_rate'] > 60:
             score += weights['bmi_resting_hr']
 
-        # Combination 4: avg_waking_respiration + resting_heart_rate (validated Awake)
-        # Article: Predictive Modeling of Heart Rate from Respiratory Signals at Rest in Young Healthy Humans
-        if (row['avg_waking_respiration'] < 12 or row['avg_waking_respiration'] > 20) and row['resting_heart_rate'] > 70:
+        # Combination 4: avg_waking_respiration + resting_heart_rate (Validated Value Article): Heart/breathing rate ratio (HBR) as a predictor of mortality in critically ill patients, 2024 Heliyon
+        # Article: ##############################################
+        # Weight article and Justification: Both breathing and heart rate are important, but the relationship between the two are not well studied. It is very uncommon for a
+        #significant disturbance of a single physiological parameter to occur in isolation. Thus, NEWS Development and Implementation Group
+        #believed multiple physiological parameters is a more robust measure of acute-illness severity than single-parameter scoring systems
+        #if (row['avg_waking_respiration'] < 12 or row['avg_waking_respiration'] > 20) and row['resting_heart_rate'] > 60:
             score += weights['avg_waking_respiration_resting_hr']
         
-        # Combination 5: Fitness Age + Resting Heart Rate (validated)
-        # Article: Resting heart rate is a population-level biomarker of cardiorespiratory fitness: The Fenland Study
-        if row['fitness_age'] > age and row['resting_heart_rate'] > 70:
+        # Combination 5: Fitness Age + Resting Heart Rate (Validated)
+        # Article: #########################################################
+        # Justification: #########################################################
+        # Weight article and Justification: RESTING HEART RATE AND MORTALITY RISK IN HYPERTENSIVE PATIENTS WITH NO ATRIAL FIBRILLATION, Journal of Hypertension 2024: Collectively, these peer-reviewed studies demonstrate that combining fitness level (or “fitness age” derived from VO₂max) and resting heart rate yields a stronger predictive insight into CVD risk than either alone; Protective Role of Resting Heart Rate on All-Cause and Cardiovascular Disease Mortality, Mayo Clinic Proceedings 2013: In
+        #practical terms, assessing both a person’s cardiorespiratory fitness (VO₂max/fitness age) and resting
+        #heart rate can improve CVD risk stratification. High-risk individuals may be more accurately identified
+        #when these two features are evaluated together, as demonstrated by hazard ratios and effect sizes in
+        #the studies above.
+        if row['fitness_age'] > age and row['resting_heart_rate'] > 60:
             score += weights['fitness_age_resting_hr']
 
-        # Combination 6: sleep_avg_spo2 + sleep_time (validated)
-        # Article: Respiratory effort during sleep and the rate of prevalent type 2 diabetes in obstructive sleep apnoea
+        #  Respiratory effort during sleep and the rate of prevalent type 2 diabetes in obstructive sleep apnoea (T2D) article
+        # Combination 6: sleep_avg_spo2 + sleep_time (validated) 
+        # Article: #################################################
+        # Justification:
+        # Weight article and Justification:
         if row['avg_sleep_spo2'] < 95.0 and (row['sleep_time_sec'] / 3600 < 6 or row['sleep_time_sec'] / 3600 > 9):
             score += weights['sleep_avg_spo2_sleep_time']
 
         #individual variables
-        #steps
-        if row['steps'] < 5000:
+        # steps (Validated value Article): Do the associations of daily steps with mortality and incident cardiovascular disease differ by sedentary time levels? A device-based cohort study British Journal of Sports Medicine, 2024; 
+        # Aricle: Prospective Association of Daily Steps With Cardiovascular Disease: A Harmonized Meta-Analysis
+        # Justification: Increased daily steps inversely correlate with CVD risk.
+        # Weight article and Justification: Same article. Moderate-to-high evidence supporting physical activity as a protective cardiovascular factor, thus moderately weighted.
+        if row['steps'] < 4500:
             score += weights['steps']
 
-        #hydration_ml 
-        if row['hydration_ml'] < 1250:
+        # hydration_ml (Validated Value Article): Fluid and Water Balance: A Scoping Review for the Nordic Nutrition Recommendations, Foods;recommend an adequate intake (AI) of 2.0 liters/day for women and 2.5 liters/day for men, including water from all sources. Given that about 20–30% of water intake comes from food, this translates to a fluid intake of approximately 1.5 to 2.0 liters/day.
+        # Article: Middle age serum sodium levels in the upper part of normal range and risk of heart failure, Middle-age high normal serum sodium as a risk factor for accelerated biological aging, chronic diseases, and premature mortality
+        # Justification: The article underscores the importance of maintaining good hydration throughout life to potentially slow down the decline in cardiac function and decrease the prevalence of heart failure. While the study does not establish a direct causal relationship, it suggests that poor hydration may increase long-term CVD and heart failure risk.
+        # Weight article and Justification: ######################################
+        if row['hydration_ml'] < 1500:
             score += weights['hydration_ml']
-        #avg_spo2
+        
+        # avg_spo2 (Validated Value Article): Effect of a lower target oxygen saturation range on the risk of hypoxaemia and elevated NEWS2 scores at a university hospital: a retrospective study BMJ Open Respiratory ResearchOpen Access; Non-contact oxygen saturation monitoring for wound healing process using dual-wavelength simultaneous acquisition imaging system Biomedical Engineering Letters (Springer, 2023)
+        # Article: ######################################
+        # Justification: ######################################
+        # Weight article and Justification: ######################################
         if row['avg_spo2'] < 95.0:
             score += weights['avg_spo2']
 
-        #avg_waking_respiration 
+        # avg_waking_respiration (Validated Value Article): Respiratory rate and its associations with disease and lifestyle factors in the general population – results from the KORA-FF4 study PloS one, 2025;
+        # Article: Respiratory rate predicts outcome after acute myocardial infarction: a prospective cohort study
+        # Justification: Elevated respiration rate predicts higher cardiovascular risk.
+        # Weight article and Justification: ######################################
         if (row['avg_waking_respiration'] < 12 or row['avg_waking_respiration'] > 20):
             score += weights['avg_waking_respiration']
 
-        #Sleep Resting Heart Rate
-        if row['sleep_resting_heart_rate'] < 30 or row['sleep_resting_heart_rate'] > 60:
+        #Sleep Resting Heart Rate (Validated Value Article) Measure by measure: Resting heart rate across the 24-hour cycle (no scopus), Resting heart rate is a population-level biomarker of cardiorespiratory fitness: The Fenland Study
+        # Article: Nighttime heart rate and cardiovascular mortality in ICD patients
+        # Justification: Higher nighttime heart rate increases CVD mortality risk.
+        # Weight article and Justification: Resting Heart Rate as a Cardiovascular Risk Factor in Hypertensive Patients: An Update. Indicates cardiovascular recovery; predictive but weaker in isolation compared to broader combinations.
+        if row['sleep_resting_heart_rate'] < 40 or row['sleep_resting_heart_rate'] > 60:
             score += weights['sleep_resting_hr']
 
-        #Sleep Average Respiration
-        if (row['sleep_avg_respiration'] < 11 or row['sleep_avg_respiration'] > 18):
+        #Sleep Average Respiration (Validated value article): Mean nocturnal respiratory rate predicts cardiovascular and all-cause mortality in community-dwelling older men and women European Respiratory Journal, 2019;
+        # Article: Association between nighttime heart rate and cardiovascular mortality in patients with implantable cardioverter-defibrillator: A cohort study
+        # Justification: Elevated nocturnal respiration rate is associated with higher CVD risk.
+        # Weight article and Justification: Same article. Respiratory effort during sleep and prevalent hypertension in obstructive sleep apnoea. Moderate standalone predictor of cardiovascular and autonomic health, especially related to nocturnal cardiovascular stress.
+        if (row['sleep_avg_respiration'] <= 14 or row['sleep_avg_respiration'] >= 16):
             score += weights['sleep_avg_respiration']
 
         # Assign the risk score to the 'cvd_risk_score' column for the current row
         df.at[index, 'cvd_risk_score'] = score
-
 
         if score <= 0.3:
             df.at[index, 'cvd_risk'] = 'Low'
@@ -190,7 +227,10 @@ def preprocess_data(df):
         else:
             df.at[index, 'cvd_risk'] = 'High'
 
-    print(df)
+    #Instead of using Label Encoder which assigns values on alphabetic order, i create a mapping dictionary with the order i want
+    risk_mapping = {'Low': 0, 'Medium': 1, 'High': 2}
+        
+    df['cvd_risk'] = df['cvd_risk'].map(risk_mapping)
     
     # Dependent features 
     #max_respiration
@@ -238,8 +278,7 @@ def preprocess_data(df):
     numeric_cols = df.select_dtypes(include='number').columns
     df_numeric = df[numeric_cols]
 
-    # Apply RobustScaler
-    #TODO Depois de normalizar os dados, criar box plot que inclua todas as features.
+    # Apply RobustScaler to the features
     scaler = RobustScaler()
     df_scaled = pd.DataFrame(scaler.fit_transform(df_numeric), columns=numeric_cols)
     
@@ -273,11 +312,11 @@ def preprocess_data(df):
     plt.show()
 
     # Apply LOF unsupervised
-    lof = LocalOutlierFactor(n_neighbors=5, contamination=0.1, novelty=False)
-    outlier_flags = lof.fit_predict(df_scaled)
+    #lof = LocalOutlierFactor(n_neighbors=5, contamination=0.1, novelty=False)
+    #outlier_flags = lof.fit_predict(df_scaled)
     
     # Add the results to DataFrame (outlier = -1)
-    df['outlier'] = outlier_flags
+    #df['outlier'] = outlier_flags
 
     # Plot and color-code by outlier status
     # sns.scatterplot(data=df, x='steps', y='vo2_max_precise', hue='outlier', palette={1: "blue", -1: "red"})
